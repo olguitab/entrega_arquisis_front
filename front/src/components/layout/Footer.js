7685d04b3ada37f19bfb8a65961da857caa0d927
ralevi02 <raimundo.lecaros@uc.cl>
2024-09-26 12:00:36 -0300

    minor changes (background, footbar, forms)

---
+++ front/src/components/layout/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <p>&copy; {new Date().getFullYear()} - Grupo 23 / Arquitectura de Software.</p>
+        </footer>
+    );
+};
+
+export default Footer;
