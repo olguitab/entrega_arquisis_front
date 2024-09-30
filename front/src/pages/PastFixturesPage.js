08081e5ab133d3a6edda1127b06030f2ccc0d6d4
ralevi02 <raimundo.lecaros@uc.cl>
2024-09-30 20:53:50 -0300

    pastFixtures update

--- front/src/pages/PastFixturesPage.js
+++ front/src/pages/PastFixturesPage.js
@@ -1,14 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import { getFixtures } from '../utils/api';
 import '../styles/Fixtures.css';
+import BondDetails from '../components/bonds/BondDetails'; 
+import Modal from '../components/layout/Modal'; 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
-const PastFixturesPage = () => {
+const FixturesPage = () => {
   const [fixtures, setFixtures] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [selectedFixture, setSelectedFixture] = useState(null);
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
   const [leagueFilter, setLeagueFilter] = useState('');
   const [dateFilter, setDateFilter] = useState('');
   const [destinationFilter, setDestinationFilter] = useState('');
+  const [localTeamFilter, setLocalTeamFilter] = useState('');
+  const [localVisitFilter, setLocalVisitFilter] = useState('');
   const [oddsAvailableFilter, setOddsAvailableFilter] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage, setItemsPerPage] = useState(5);
@@ -28,6 +37,20 @@ const PastFixturesPage = () => {
     loadFixtures();
   }, []);
 
+  const handleShowModal = (fixture) => {
+    setSelectedFixture(fixture);
+    setModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
+    setSelectedFixture(null);
+  };
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!isDropdownOpen); // Abrir o cerrar el dropdown
+  };
+
   const handleLeagueChange = (event) => {
     setLeagueFilter(event.target.value);
   };
@@ -40,23 +63,44 @@ const PastFixturesPage = () => {
     setDestinationFilter(event.target.value);
   };
 
+  const handleLocalTeamChange = (event) => {
+    setLocalTeamFilter(event.target.value);
+  };
+
+  const handleVisitTeamChange = (event) => {
+    setLocalVisitFilter(event.target.value);
+  };
+
+  const handleOddsAvailableChange = (event) => {
+    setOddsAvailableFilter(event.target.checked);
+  };
+
   const handleItemsPerPageChange = (event) => {
     setItemsPerPage(parseInt(event.target.value));
     setCurrentPage(1); // Reset to first page
   };
 
   const filteredFixtures = fixtures
-    .filter(fixture => fixture.fixture.status.long !== 'Not Started')
+    .filter(fixture => fixture.fixture.status.long != 'Not Started')
     .filter(fixture => 
       leagueFilter === '' || 
       fixture.league.name.toLowerCase().includes(leagueFilter.toLowerCase())
     )
+    
     .filter(fixture => dateFilter === '' || new Date(fixture.fixture.date).toISOString().slice(0, 10) === dateFilter)
     .filter(fixture => 
       destinationFilter === '' || 
-      fixture.league.country.toLowerCase().includes(destinationFilter.toLowerCase()) || // Filtra por país
-      fixture.teams.home.name.toLowerCase().includes(destinationFilter.toLowerCase())   // Filtra por equipo local
+      fixture.league.country.toLowerCase().includes(destinationFilter.toLowerCase())
     )
+    .filter(fixture =>
+      localTeamFilter === '' ||
+      fixture.teams.home.name.toLowerCase().includes(localTeamFilter.toLowerCase())
+    )
+    .filter(fixture =>
+      localVisitFilter === '' ||
+      fixture.teams.away.name.toLowerCase().includes(localVisitFilter.toLowerCase())
+    )
+    .filter(fixture => !oddsAvailableFilter || (fixture.odds && fixture.odds[0] && fixture.odds[0].values.length > 0));
 
   // Pagination
   const indexOfLastFixture = currentPage * itemsPerPage;
@@ -74,65 +118,98 @@ const PastFixturesPage = () => {
 
   return (
     <div className="fixtures-page">
-      <h1>Past Fixtures</h1>
+      <h1>Upcoming Fixtures</h1>
 
-      {/* Filtros */}
-      <div className="filters">
-        <div className="filter-item">
-          <label className="filter-label">
-            Filter by League:
-            <input 
-              type="text" 
-              value={leagueFilter} 
-              onChange={handleLeagueChange} 
-              placeholder="Enter league name" 
-            />
-          </label>
-        </div>
+      {/* Dropdown para los filtros */}
+      <div className="filter-dropdown">
+        <button className="filter-dropdown-button" onClick={toggleDropdown}>
+          {isDropdownOpen ? 'Hide Filters' : 'Show Filters'}
+        </button>
+        <div
+          className={`dropdown-content ${isDropdownOpen ? 'open' : ''}`}
+          style={{ maxHeight: isDropdownOpen ? '1000px' : '0px' }}
+        >
+          <div className="filter-title">Filter By:</div>
+            <div className="filter-item">
+              <label className="filter-label">
+                <div className="filter-description">League:</div>
+                <input 
+                  type="text" 
+                  value={leagueFilter} 
+                  onChange={handleLeagueChange} 
+                  placeholder="Enter league name" 
+                />
+              </label>
+            </div>
 
-        <div className="filter-item">
-          <label className="filter-label">
-            Filter by Date:
-            <input 
-              type="date" 
-              value={dateFilter} 
-              onChange={handleDateChange} 
-            />
-          </label>
-        </div>
+            <div className="filter-item">
+              <label className="filter-label">
+              <div className="filter-description">Date:</div>
+                <input 
+                  type="date" 
+                  value={dateFilter} 
+                  onChange={handleDateChange} 
+                />
+              </label>
+            </div>
 
-        <div className="filter-item">
-          <label className="filter-label">
-            Filter by Destination:
-            <input 
-              type="text" 
-              value={destinationFilter} 
-              onChange={handleDestinationChange} 
-              placeholder="Enter destination" 
-            />
-          </label>
-        </div>
-      </div>
+            <div className="filter-item">
+              <label className="filter-label">
+              <div className="filter-description">Destination:</div>
+                <input 
+                  type="text" 
+                  value={destinationFilter} 
+                  onChange={handleDestinationChange} 
+                  placeholder="Enter destination" 
+                />
+              </label>
+            </div>
 
-      {/* Resultados encontrados */}
-      <div className="results-info">
-        <div className="items-per-page">
-          <label className="filter-label">
-            Items per Page:
-            <select value={itemsPerPage} onChange={handleItemsPerPageChange} className="select">
-              <option value="5">5</option>
-              <option value="10">10</option>
-              <option value="15">15</option>
-              <option value="20">20</option>
-              <option value="25">25</option>
-            </select>
-          </label>
-        </div>
-        <p>{filteredFixtures.length} results found. Showing {indexOfFirstFixture + 1}–{Math.min(indexOfLastFixture, filteredFixtures.length)} of {filteredFixtures.length}</p>
+            <div className="filter-item">
+              <label className="filter-label">
+              <div className="filter-description">Local Team:</div>                
+                <input 
+                  type="text" 
+                  value={localTeamFilter} 
+                  onChange={handleLocalTeamChange} 
+                  placeholder="Enter local team name" 
+                />
+              </label>
+            </div>
+
+            <div className="filter-item">
+              <label className="filter-label">
+              <div className="filter-description">Visit Team:</div>
+                <input 
+                  type="text" 
+                  value={localVisitFilter} 
+                  onChange={handleVisitTeamChange} 
+                  placeholder="Enter visit team name" 
+                />
+              </label>
+            </div>
+
+            {/* Paginación y resultados */}
+            <div className="results-info">
+              <div className="items-per-page">
+                <label className="filter-label">
+                  Items per Page:
+                  <select value={itemsPerPage} onChange={handleItemsPerPageChange} className="select">
+                    <option value="5">5</option>
+                    <option value="10">10</option>
+                    <option value="15">15</option>
+                    <option value="20">20</option>
+                    <option value="25">25</option>
+                  </select>
+                </label>
+              </div>
+              <p>{filteredFixtures.length} results found. Showing {indexOfFirstFixture + 1}–{Math.min(indexOfLastFixture, filteredFixtures.length)} of {filteredFixtures.length}</p>
+            </div>
+          </div>
       </div>
 
       <ul>
-        {currentFixtures.map((fixture) => (
+      {currentFixtures.map((fixture) => (
           <li key={fixture.fixture.id} className="fixture-item">
             <div className="fixture-details">
               <div className="team-info">
@@ -166,9 +243,8 @@ const PastFixturesPage = () => {
                   <div className="failed-text">No odds available</div>
                 )}
               </div>
-              <div className="separator"></div>
-
               <div className="result">
+                <div className="separator"></div>
                 Goals:
                 <div className="scores"> 
                   <div className="score">
@@ -185,20 +261,25 @@ const PastFixturesPage = () => {
         ))}
       </ul>
 
-      {/* Paginación */}
       <div className="pagination">
-        {Array.from({ length: totalPages }, (_, index) => (
-          <button 
-            key={index + 1} 
-            className={`page-button ${currentPage === index + 1 ? 'active' : ''}`} 
-            onClick={() => setCurrentPage(index + 1)}
-          >
-            {index + 1}
+        {currentPage > 1 && (
+          <button className="pagination-button" onClick={() => setCurrentPage(currentPage - 1)}>
+            <FontAwesomeIcon icon={faChevronLeft} className="pagination-icon" />
           </button>
-        ))}
+        )}
+        <span>Page {currentPage} of {totalPages}</span>
+        {currentPage < totalPages && (
+          <button className="pagination-button" onClick={() => setCurrentPage(currentPage + 1)}>
+            <FontAwesomeIcon icon={faChevronRight} className="pagination-icon" />
+          </button>
+        )}
       </div>
+
+      <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
+        {selectedFixture && <BondDetails fixture={selectedFixture} />}
+      </Modal>
     </div>
   );
 };
 
-export default PastFixturesPage;
+export default FixturesPage;
